35b2614c3f98f61b11ee65a4fe1f7268700e6650
adogbeji <baka_1993@yahoo.co.uk>
2023-06-19 20:43:21 +0100

    Change font size & colour of text

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,18 @@ void main() {
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 165, 47, 186),
-                Color.fromARGB(255, 176, 94, 191),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight
-            ),
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 165, 47, 186),
+              Color.fromARGB(255, 176, 94, 191),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
           ),
           child: const Center(
-            child: Text('Hello World!'),
+            child: Text('Hello World!',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.white,
+              ),
+            ),
           ),
         ),
       ),
