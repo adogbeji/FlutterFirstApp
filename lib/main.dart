385d3fe37f946857984799c2ec5ef4615f4f7f18
adogbeji <baka_1993@yahoo.co.uk>
2023-07-23 18:40:22 +0100

    Make constructor function const

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
 }
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer({super.key});
+  const GradientContainer({super.key});
 
   @override
   Widget build(context) {
