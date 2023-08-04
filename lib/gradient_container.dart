7b35cede54909159a7bcbbe55d8394bffc455964
adogbeji <baka_1993@yahoo.co.uk>
2023-08-04 20:44:49 +0100

    Use dart fix

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -8,15 +8,19 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(context) {
     return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 165, 47, 186),
-              Color.fromARGB(255, 176, 94, 191),
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-          ),
-          child: const Center(
-            child: StyledText(),
-          ),
-        );
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 165, 47, 186),
+            Color.fromARGB(255, 176, 94, 191),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
   }
 }
