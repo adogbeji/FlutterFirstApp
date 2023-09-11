dce9dce661cf113961903b40b4df84b364b88022
adogbeji <baka_1993@yahoo.co.uk>
2023-09-11 20:36:59 +0100

    Make variables final

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:first_app/styled_text.dart';
 
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+final startAlignment = Alignment.topLeft;
+final endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
