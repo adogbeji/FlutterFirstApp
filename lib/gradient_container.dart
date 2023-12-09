a18463ea18fe3396a3debde3f2aa7087044e2b56
adogbeji <baka_1993@yahoo.co.uk>
2023-12-09 20:53:16 +0000

    Change font size & adding padding to TextButton in GradientContainer

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -38,7 +38,11 @@ class GradientContainer extends StatelessWidget {
             TextButton(
               onPressed: rollDice,
               style: TextButton.styleFrom(
+                padding: const EdgeInsets.only(top: 20,),
                 foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
               ),
               child: const Text('Roll Dice')
             ),
